5d4a56af6170af2fd430b7e85d6333344cca327f
Michael Ansbro <mransbro@gmail.com>
2022-02-23 16:52:38 +0000

    remove unused modules

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from app import app
 from app.tools import create_db
-from os import environ
 from os.path import exists
-import webbrowser
-from waitress import serve
 
 
 def main():
@@ -12,10 +9,6 @@ def main():
         create_db()
         print("Database initialized")
 
-    # auto-open the application
-    # webbrowser.open("http://0.0.0.0:8080/")
-    # port = int(environ.get("PORT", 8080))
-    # serve(app, port=port)
     app.run(host="0.0.0.0")
 
 
