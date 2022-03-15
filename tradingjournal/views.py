ea317069732b485a1bb41fc89374efec615e15e9
Michael Ansbro <michael.ansbro@moneysupermarket.com>
2022-03-15 16:27:18 +0000

    removed all unfinished features

--- tradingjournal/views.py
+++ tradingjournal/views.py
@@ -1,8 +1,8 @@
-from importlib_metadata import entry_points
+
 from .tools import allowed_file, csv_import
-from .models import WeeklyRoutine, DailyRoutine, Trade, db
-from .forms import DailyForm, WeeklyForm, TradeForm, RiskCalculator, UpdateTradeForm
-from flask import g, render_template, flash, request, redirect, url_for
+from .models import Trade, db
+from .forms import TradeForm, UpdateTradeForm
+from flask import render_template, flash, request, redirect, url_for
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from werkzeug.utils import secure_filename
@@ -58,99 +58,6 @@ def index():
         closedtrades=closedtrades,
     )
 
-
-@app.route("/dailyroutine/add", methods=["GET", "POST"])
-@db_add_limit
-def add_dailyroutine():
-    """
-    Return exisiting and add new daily routine entries.
-    """
-
-    form = DailyForm()
-
-    if form.validate_on_submit():
-
-        record = DailyRoutine(
-            date=datetime.strptime(form.date.data, dateformat),
-            stocks_above_20ma=form.stocks_above_20ma.data,
-            stocks_above_50ma=form.stocks_above_50ma.data,
-            stocks_above_200ma=form.stocks_above_200ma.data,
-        )
-
-        db.session.add(record)
-        db.session.commit()
-
-        message = f"The data for {record.date} has been submitted."
-
-        return render_template("add_dailyroutine.html", message=message, form=form)
-
-    else:
-        for field, errors in form.errors.items():
-            for error in errors:
-                flash(
-                    "Error in {}: {}".format(getattr(form, field).label.text, error),
-                    "error",
-                )
-
-    return render_template("add_dailyroutine.html", form=form, title="Daily Routine")
-
-
-@app.route("/dailyroutine/data")
-def daily_data():
-    """
-    Return daily routine table data.
-    """
-    return {
-        "data": [dailyroutine.to_dict() for dailyroutine in DailyRoutine.query.all()]
-    }
-
-
-@app.route("/weeklyroutine/add", methods=["GET", "POST"])
-@db_add_limit
-def add_weeklyroutine():
-    """
-    Return exisiting and add new weekly routine entries.
-    """
-
-    form = WeeklyForm()
-
-    if form.validate_on_submit():
-
-        record = WeeklyRoutine(
-            date=datetime.strptime(form.date.data, dateformat),
-            industry_groups=form.industry_groups.data,
-            scans=form.scans.data,
-            watchlist=form.watchlist.data,
-            focuslist=form.focuslist.data,
-            open_positions=form.open_positions.data,
-        )
-
-        db.session.add(record)
-        db.session.commit()
-
-        message = f"The data for {record.date} has been submitted."
-
-        return render_template("add_weeklyroutine.html", message=message, form=form)
-
-    else:
-        for field, errors in form.errors.items():
-            for error in errors:
-                flash(
-                    "Error in {}: {}".format(getattr(form, field).label.text, error),
-                    "error",
-                )
-
-    return render_template("add_weeklyroutine.html", form=form, title="Weekly Routine")
-
-
-@app.route("/weeklyroutine/data")
-def weekly_data():
-    """
-    Return weekly routine table data.
-    """
-    return {
-        "data": [weeklyroutine.to_dict() for weeklyroutine in WeeklyRoutine.query.all()]
-    }
 
 
 @app.route("/trade/add", methods=["GET", "POST"])
@@ -297,45 +204,6 @@ def delete_trade():
 
     return redirect(url_for("index"))
 
-
-# @app.route("/trade/data")
-# def trade_data():
-#    """
-#    Return trade table data.
-#    """
-#
-#    return {"data": [trade.to_dict() for trade in Trade.query.all()]}
-
-
-@app.route("/risk", methods=["GET", "POST"])
-def risk_calculator():
-    """
-    Risk calculator
-    """
-
-    form = RiskCalculator()
-
-    if request.method == "POST":
-
-        if form.validate_on_submit():
-            account_value = form.account_value.data
-            max_risk = form.max_risk.data
-            entry_price = form.entry_price.data
-            stop = form.stop.data
-            position_size = round(account_value / 100 * max_risk, 2)
-            num_shares = round(position_size / entry_price, 2)
-            risk_per_share = 0.0
-            risk_account_percent = 0
-            risk_account_value = 0.0
-
-        form = RiskCalculator()
-
-        return render_template(
-            "risk_calculator.html",
-            form=form,
-        )
-
-    return render_template("risk_calculator.html", form=form)
 
 
 @app.errorhandler(404)
