23ae771366637bceef54555053ac08f62136ce0d
Michael Ansbro <michael.ansbro@moneysupermarket.com>
2022-03-16 13:46:51 +0000

    adding risk calculator

--- tradingjournal/views.py
+++ tradingjournal/views.py
@@ -1,7 +1,7 @@
 
 from .tools import allowed_file, csv_import
 from .models import Trade, db
-from .forms import TradeForm, UpdateTradeForm
+from .forms import TradeForm, UpdateTradeForm, RiskCalculator
 from flask import render_template, flash, request, redirect, url_for
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -205,6 +205,39 @@ def delete_trade():
     return redirect(url_for("index"))
 
 
+@app.route("/risk", methods=["GET", "POST"])
+def risk_calculator():
+    """
+    Risk calculator
+    """
+
+    form = RiskCalculator()
+    risk={}
+
+    if request.method == "POST":
+
+        if form.validate_on_submit():
+            account_value = form.account_value.data
+            max_risk = form.max_risk.data
+            entry_price = form.entry_price.data
+            stop = form.stop.data
+            
+            account_risk = account_value / 100 * max_risk
+            trade_risk = entry_price - stop
+
+            risk["risk_per_share"] = round(trade_risk, 2)
+            risk["num_shares"] = round(account_risk / trade_risk, 2)
+            risk["position_size"] = round(risk["num_shares"] * entry_price, 2)
+            risk["risk_per_share_percent"] = round((risk["risk_per_share"] / entry_price) * 100, 2)
+            risk["risk_account_value"] = round(risk["num_shares"] * risk["risk_per_share"], 2)
+
+
+        return render_template(
+            "risk_calculator.html",
+            form=form, risk=risk
+        )
+
+    return render_template("risk_calculator.html", form=form,risk=risk)
 
 @app.errorhandler(404)
 def handle_404(e):
